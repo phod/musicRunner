cf9b376ff68e2747f6b0d7a86e3ff91f83d178a1
phod <philip.hodder@gmail.com>
2015-01-31 22:29:23 +1300

    Initial commit

---
+++ desktop/src/io/github/phod/musicRunner/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package io.github.phod.musicRunner.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import io.github.phod.musicRunner.MusicRunner;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MusicRunner(), config);
+	}
+}
