fe0d35b4f4119e09711e0a67556cf1c50a612d09
phod <philip.hodder@gmail.com>
2015-02-01 00:05:19 +1300

    Added basic assets. Removed default badlogiclogo.

--- core/src/io/github/phod/musicRunner/MusicRunner.java
+++ core/src/io/github/phod/musicRunner/MusicRunner.java
@@ -8,12 +8,10 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MusicRunner extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
@@ -21,7 +19,6 @@ public class MusicRunner extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
 		batch.end();
 	}
 }
